e36973635c013e56834d2be0d0791d9b9adeac40
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-03 20:48:02 +0200

    Create a prompt from a template

--- src/main/java/dev/mout/spring_ai_demo/ChatController.java
+++ src/main/java/dev/mout/spring_ai_demo/ChatController.java
@@ -1,6 +1,11 @@
 package dev.mout.spring_ai_demo;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +19,9 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
+    @Value("classpath:/prompts/synonyms.st")
+    private Resource synonymPromptResource;
+
     public ChatController(ChatClient.Builder builder) {
         this.chatClient = builder.build();
     }
@@ -22,5 +30,17 @@ public class ChatController {
     public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         String generation = chatClient.prompt(message).call().content();
         return Map.of("generation", requireNonNull(generation));
+    }
+
+    @GetMapping("/synonyms")
+    public String synonyms(@RequestParam(value = "word", defaultValue = "eating") String word) {
+        PromptTemplate template = new PromptTemplate(synonymPromptResource);
+        Prompt prompt = template.create(Map.of("word", word));
+
+        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
+        requireNonNull(response);
+        requireNonNull(response.getResult());
+
+        return response.getResult().getOutput().getText();
     }
 }
