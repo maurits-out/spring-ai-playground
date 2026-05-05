12edf6975c97fa13a5192246fc3414a959b461f3
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-05 17:05:33 +0200

    Playing with output converters.

---
+++ src/main/java/dev/mout/spring_ai_demo/Artist.java
@@ -0,0 +1,6 @@
+package dev.mout.spring_ai_demo;
+
+import java.util.List;
+
+public record Artist(String author, List<String> songs) {
+}
--- src/main/java/dev/mout/spring_ai_demo/ChatController.java
+++ src/main/java/dev/mout/spring_ai_demo/ChatController.java
@@ -4,10 +4,13 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,6 +59,51 @@ public class ChatController {
         PromptTemplate template = new PromptTemplate(message);
         Map<String, Object> additionalVariables = Map.of(
                 "author", author,
+                "format", converter.getFormat()
+        );
+        Prompt prompt = template.create(additionalVariables);
+        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
+
+        requireNonNull(response);
+        requireNonNull(response.getResult());
+        requireNonNull(response.getResult().getOutput().getText());
+
+        return converter.convert(response.getResult().getOutput().getText());
+    }
+
+    @GetMapping("/artist/{artist}")
+    public Map<String, Object> getArtistsSocialLinks(@PathVariable("artist") String artist) {
+        var message = """
+                Generate a list of links for the artist {artist}. Include the name of the artist as the key and any social network links as the object.
+                {format}
+                """;
+        MapOutputConverter converter = new MapOutputConverter();
+        PromptTemplate template = new PromptTemplate(message);
+        Map<String, Object> additionalVariables = Map.of(
+                "artist", artist,
+                "format", converter.getFormat()
+        );
+        Prompt prompt = template.create(additionalVariables);
+        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
+
+        requireNonNull(response);
+        requireNonNull(response.getResult());
+        requireNonNull(response.getResult().getOutput().getText());
+
+        return converter.convert(response.getResult().getOutput().getText());
+    }
+
+    @GetMapping("/by-artist")
+    public Artist getSongsByArtist(@RequestParam(value = "artist", defaultValue = "Earth, wind and fire") String artist) {
+        String message = """
+                Generate a list of songs performed by the artist {artist}. If you aren't positive that a song
+                belongs to this artist then don't include it.
+                {format}
+                """;
+        BeanOutputConverter<Artist> converter = new BeanOutputConverter<>(Artist.class);
+        PromptTemplate template = new PromptTemplate(message);
+        Map<String, Object> additionalVariables = Map.of(
+                "artist", artist,
                 "format", converter.getFormat()
         );
         Prompt prompt = template.create(additionalVariables);
