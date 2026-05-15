c81fb3207bf5b4d82cdd8fe7ea33422b098c7fc6
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-15 21:32:13 +0200

    Refactoring.

---
+++ src/main/java/dev/mout/spring_ai_demo/ArtistController.java
@@ -0,0 +1,46 @@
+package dev.mout.spring_ai_demo;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 3b. Converting the output to a map.
+ */
+@RestController
+final class ArtistController {
+
+    private final ChatClient chatClient;
+    private final MapOutputConverter converter = new MapOutputConverter();
+    private final String message = """
+            Generate a list of links for the artist {artist}. Include the name of the artist as the key and any social network links as the object.
+            {format}
+            """;
+
+    public ArtistController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/artist/{artist}")
+    public Map<String, Object> getArtistsSocialLinks(@PathVariable("artist") String artist) {
+        PromptTemplate template = new PromptTemplate(message);
+        Prompt prompt = template.create(additionalVariables(artist));
+        String content = chatClient.prompt(prompt).call().content();
+        return converter.convert(Objects.requireNonNull(content));
+    }
+
+    private @NonNull Map<String, Object> additionalVariables(String artist) {
+        return Map.of(
+                "artist", artist,
+                "format", converter.getFormat()
+        );
+    }
+}
--- src/main/java/dev/mout/spring_ai_demo/ChatController.java
+++ src/main/java/dev/mout/spring_ai_demo/ChatController.java
@@ -5,9 +5,7 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.converter.BeanOutputConverter;
-import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,28 +20,6 @@ public class ChatController {
 
     public ChatController(ChatClient.Builder builder) {
         this.chatClient = builder.build();
-    }
-
-    @GetMapping("/artist/{artist}")
-    public Map<String, Object> getArtistsSocialLinks(@PathVariable("artist") String artist) {
-        var message = """
-                Generate a list of links for the artist {artist}. Include the name of the artist as the key and any social network links as the object.
-                {format}
-                """;
-        MapOutputConverter converter = new MapOutputConverter();
-        PromptTemplate template = new PromptTemplate(message);
-        Map<String, Object> additionalVariables = Map.of(
-                "artist", artist,
-                "format", converter.getFormat()
-        );
-        Prompt prompt = template.create(additionalVariables);
-        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
-
-        requireNonNull(response);
-        requireNonNull(response.getResult());
-        requireNonNull(response.getResult().getOutput().getText());
-
-        return converter.convert(response.getResult().getOutput().getText());
     }
 
     @GetMapping("/by-artist")
