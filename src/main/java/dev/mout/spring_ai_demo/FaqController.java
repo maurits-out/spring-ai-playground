74f4745aa5100f6664cd879f9e830b9331b47630
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-15 16:55:00 +0200

    Playing with RAG.

---
+++ src/main/java/dev/mout/spring_ai_demo/FaqController.java
@@ -0,0 +1,56 @@
+package dev.mout.spring_ai_demo;
+
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStoreRetriever;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class FaqController {
+
+    private final ChatClient chatClient;
+    private final VectorStoreRetriever vectorStoreRetriever;
+    private final Resource templateResource;
+
+    public FaqController(ChatClient.Builder builder,
+                         VectorStoreRetriever vectorStoreRetriever,
+                         @Value("classpath:/prompts/rag-prompt-template.st") Resource templateResource) {
+        this.chatClient = builder.build();
+        this.vectorStoreRetriever = vectorStoreRetriever;
+        this.templateResource = templateResource;
+    }
+
+    @GetMapping("/faq")
+    public String prompt(@RequestParam(value = "message", defaultValue = "How can I buy tickets for the Olympic Games Paris 2024") String message) {
+        SearchRequest request = buildSearchRequest(message);
+        List<Document> documents = vectorStoreRetriever.similaritySearch(request);
+        PromptTemplate template = new PromptTemplate(templateResource);
+        Map<String, Object> additionalVariables = createAdditionalVariables(message, documents);
+        Prompt prompt = template.create(additionalVariables);
+        return chatClient.prompt(prompt).call().content();
+    }
+
+    private @NonNull Map<String, Object> createAdditionalVariables(String message, List<Document> documents) {
+        List<@Nullable String> contentList = documents.stream().map(Document::getText).toList();
+        return Map.of(
+                "input", message,
+                "documents", String.join("\n", contentList)
+        );
+    }
+
+    private @NonNull SearchRequest buildSearchRequest(String message) {
+        return SearchRequest.builder().query(message).topK(1).build();
+    }
+}
---
+++ src/main/java/dev/mout/spring_ai_demo/config/RagConfiguration.java
@@ -0,0 +1,74 @@
+package dev.mout.spring_ai_demo.config;
+
+import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStoreRetriever;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Configuration
+public class RagConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(RagConfiguration.class);
+
+    @Value("classpath:/docs/olympic-faq.txt")
+    private Resource faq;
+
+    @Value("vectorstore.json")
+    private String vectorStoreName;
+
+    @Bean
+    TokenTextSplitter tokenTextSplitter() {
+        return TokenTextSplitter.builder().build();
+    }
+
+    @Bean
+    VectorStoreRetriever vectorStoreRetriever(EmbeddingModel embeddingModel, TokenTextSplitter splitter) {
+        SimpleVectorStore store = constructSimpleVectorStore(embeddingModel);
+        File file = getVectorStorePath();
+        if (file.exists()) {
+            loadFromFile(store, file);
+        } else {
+            createFromDocument(store, file, splitter);
+        }
+        return store;
+    }
+
+    private void createFromDocument(SimpleVectorStore store, File file, TokenTextSplitter splitter) {
+        log.info("Creating new Vector Store from document");
+        List<Document> splitDocuments = loadAndSplitDocument(splitter);
+        store.add(splitDocuments);
+        store.save(file);
+    }
+
+    private @NonNull List<Document> loadAndSplitDocument(TokenTextSplitter splitter) {
+        TextReader reader = new TextReader(faq);
+        List<Document> documents = reader.get();
+        return splitter.apply(documents);
+    }
+
+    private void loadFromFile(SimpleVectorStore store, File file) {
+        log.info("Loading Vector Store from file {}", file);
+        store.load(file);
+    }
+
+    private @NonNull SimpleVectorStore constructSimpleVectorStore(EmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+
+    private File getVectorStorePath() {
+        return Paths.get(System.getProperty("user.home"), vectorStoreName).toFile();
+    }
+}
