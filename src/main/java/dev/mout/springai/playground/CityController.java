b84788352f6f92a7b40065a6e4eabb4f1ea37a04
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-23 12:05:34 +0200

    Supply system message in the builder.

--- src/main/java/dev/mout/springai/playground/CityController.java
+++ src/main/java/dev/mout/springai/playground/CityController.java
@@ -1,14 +1,11 @@
 package dev.mout.springai.playground;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 import static dev.mout.springai.playground.tools.WeatherTools.CURRENT_WEATHER_TOOL;
 
@@ -19,18 +16,20 @@ import static dev.mout.springai.playground.tools.WeatherTools.CURRENT_WEATHER_TO
 @RestController
 final class CityController {
 
-    private static final SystemMessage SYSTEM_MESSAGE = new SystemMessage("You are a helpful AI assistant answering questions about cities around the world");
+    private static final String SYSTEM_MESSAGE = "You are a helpful AI assistant answering questions about cities around the world";
 
     private final ChatClient chatClient;
 
     public CityController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+        this.chatClient = builder
+                .defaultSystem(SYSTEM_MESSAGE)
+                .build();
     }
 
     @GetMapping("/cities")
     public String cities(@RequestParam(value = "message") String message) {
         UserMessage userMessage = new UserMessage(message);
-        Prompt prompt = new Prompt(List.of(SYSTEM_MESSAGE, userMessage));
+        Prompt prompt = new Prompt(userMessage);
         return chatClient
                 .prompt(prompt)
                 .toolNames(CURRENT_WEATHER_TOOL)
