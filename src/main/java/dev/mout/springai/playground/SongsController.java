7d79ae9fe2c9ff8778209d9fdb049fa41a4cc071
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-23 12:52:32 +0200

    Enhanced controller to return a list.

--- src/main/java/dev/mout/springai/playground/SongsController.java
+++ src/main/java/dev/mout/springai/playground/SongsController.java
@@ -2,6 +2,7 @@ package dev.mout.springai.playground;
 
 import org.jspecify.annotations.NonNull;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,13 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 3c. Converting the output to a bean.
+ * 3c. Converting the output to a list of records.
  */
 @RestController
 final class SongsController {
 
     private static final String MESSAGE = """
-            Generate a list of songs performed by the artist {artist}. If you aren't positive that a song
+            Given artists {artists}. For each artist generate a list of 3 songs performed by that artists. If you aren't positive that a song
             belongs to this artist then don't include it.""";
 
     private final ChatClient chatClient;
@@ -25,16 +26,17 @@ final class SongsController {
         this.chatClient = builder.build();
     }
 
-    @GetMapping("/songs-by-artist")
-    public Artist getSongsByArtist(@RequestParam(value = "artist", defaultValue = "Earth, wind and fire") String artist) {
+    @GetMapping("/songs-by-artists")
+    public List<Artist> getSongsByArtist(@RequestParam(value = "artists", defaultValue = "U2, Paul Carrack, Mike and the mechanics, Genesis") String artist) {
         return chatClient.prompt()
                 .user(spec -> spec.text(MESSAGE).params(additionalVariables(artist)))
                 .call()
-                .entity(Artist.class);
+                .entity(new ParameterizedTypeReference<>() {
+                });
     }
 
-    private @NonNull Map<String, Object> additionalVariables(String artist) {
-        return Map.of("artist", artist);
+    private @NonNull Map<String, Object> additionalVariables(String artists) {
+        return Map.of("artists", artists);
     }
 
     public record Artist(String artist, List<String> songs) {
