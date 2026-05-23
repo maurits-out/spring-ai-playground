d98a370d90729ec01cf9eab1978b6cda4403258f
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-23 13:48:08 +0200

    Refactoring.

--- src/main/java/dev/mout/springai/playground/BestRestaurantsController.java
+++ src/main/java/dev/mout/springai/playground/BestRestaurantsController.java
@@ -3,6 +3,7 @@ package dev.mout.springai.playground;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
@@ -12,22 +13,24 @@ import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
  * 8. Return a stream.
  */
 @RestController
-final class HamburgerRestaurantsController {
+final class BestRestaurantsController {
 
     private final ChatClient chatClient;
     private final int bufferSize;
 
-    public HamburgerRestaurantsController(ChatClient.Builder builder, @Value("50") int bufferSize) {
+    public BestRestaurantsController(ChatClient.Builder builder, @Value("50") int bufferSize) {
         this.chatClient = builder.build();
         this.bufferSize = bufferSize;
     }
 
-    @GetMapping(path = "/hamburger-restaurants", produces = TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> getHamburgerRestaurants() {
+    @GetMapping(path = "/best-restaurants", produces = TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getBestRestaurants(@RequestParam("city") String city) {
         return chatClient.prompt()
-                .user(spec -> spec.text("""
-                    What are the 10 best hamburger restaurants in Amsterdam.
-                    Include a short description for each restaurant."""))
+                .system("""
+                    You are given a name of a city. Provide a list of the 10
+                    best restaurants in that city together with the address and
+                    short description.""")
+                .user(spec -> spec.text(city))
                 .stream()
                 .content()
                 .transform(this::toChunk);
