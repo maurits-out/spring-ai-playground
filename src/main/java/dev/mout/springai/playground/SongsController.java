da6ddb96527fa29e2b8646845855ec74969f4fb4
maurits <17969131+maurits-out@users.noreply.github.com>
2026-05-23 11:55:45 +0200

    Simplified converting output to a bean.

--- src/main/java/dev/mout/springai/playground/SongsController.java
+++ src/main/java/dev/mout/springai/playground/SongsController.java
@@ -4,14 +4,12 @@ import org.jspecify.annotations.NonNull;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * 3c. Converting the output to a bean.
@@ -21,12 +19,9 @@ final class SongsController {
 
     private static final String MESSAGE = """
             Generate a list of songs performed by the artist {artist}. If you aren't positive that a song
-            belongs to this artist then don't include it.
-            {format}
-            """;
+            belongs to this artist then don't include it.""";
 
     private final ChatClient chatClient;
-    private final BeanOutputConverter<Artist> converter = new BeanOutputConverter<>(Artist.class);
 
     public SongsController(ChatClient.Builder builder) {
         this.chatClient = builder.build();
@@ -36,15 +31,11 @@ final class SongsController {
     public Artist getSongsByArtist(@RequestParam(value = "artist", defaultValue = "Earth, wind and fire") String artist) {
         PromptTemplate template = new PromptTemplate(MESSAGE);
         Prompt prompt = template.create(additionalVariables(artist));
-        String content = chatClient.prompt(prompt).call().content();
-        return converter.convert(Objects.requireNonNull(content));
+        return chatClient.prompt(prompt).call().entity(Artist.class);
     }
 
     private @NonNull Map<String, Object> additionalVariables(String artist) {
-        return Map.of(
-                "artist", artist,
-                "format", converter.getFormat()
-        );
+        return Map.of("artist", artist);
     }
 
     public record Artist(String artist, List<String> songs) {
